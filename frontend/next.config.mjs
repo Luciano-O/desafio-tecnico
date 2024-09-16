aa54bcc99f874ce587923a5760a2155af0cf7b4f
Luciano Oliveira <oliveiraluck78@gmail.com>
2024-09-16 18:11:58 -0300

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'desafio-tecnico-yzpm.onrender.com'],
   },
   env: {
     API_KEY: process.env.API_KEY,
